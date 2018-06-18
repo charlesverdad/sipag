41832b1af730616bf548d3ed5b96861ab9fefc9e
Charles Verdad <charles.verdad@gmail.com>
2018-06-18 20:53:55 +0800

    add simple threaded scheduler

---
+++ sipag.py
@@ -0,0 +1,38 @@
+import threading
+from threading import Thread
+from collections import Counter
+from time import sleep
+
+from tasks import log_usage
+usage = Counter()
+
+def continuously_log():
+	global usage
+	lock = threading.Lock()
+	while True:
+		with lock:
+			log_usage(counter=usage)
+		sleep(1)
+
+def continuously_save():
+	global usage
+	lock = threading.Lock()
+	while True:
+		with lock:
+			print(usage)
+		sleep(5)
+
+logger_thread = Thread(target=continuously_log)
+logger_thread.daemon = True
+logger_thread.start()
+
+saver_thread = Thread(target=continuously_save)
+saver_thread.daemon = True
+saver_thread.start()
+
+print("heyo")
+sleep(2)
+print ('heya')
+sleep(3)
+while True:
+	pass
--- tasks.py
+++ tasks.py
@@ -1,15 +1,16 @@
 
-from collections import Counter
+import threading
+
 
 from tools.windows import get_window_name
 
-usage = Counter()
 
-def log_usage():
+
+def log_usage(counter):
 	'''
 	Increment window usage
 	'''
-	usage[get_window_name()] += 1
+	counter[get_window_name()] += 1
 
 def update_storage():
 	'''
@@ -18,3 +19,58 @@ def update_storage():
 	pass
 	# this could be a dump of `usage` into a *.json file or a full-fledged database.
 
+
+
+
+# class TaskThread(threading.Thread):
+#     """
+# 	Creates a threading.Thread object that calls `func` with 
+# 	arguments `args` every `interval` intervals.
+
+#     Adapted from http://code.activestate.com/recipes/65222-run-a-task-every-few-seconds/
+#     """
+    
+#     def __init__(self={}):
+#         threading.Thread.__init__(self)
+#         self._finished = threading.Event()
+#         self._interval = 15.0
+    
+#     def shutdown(self):
+#         """Stop this thread"""
+#         self._finished.set()
+
+#     def setInterval(self, interval):
+#         """Set the number of seconds we sleep between executing our task"""
+#         self._interval = interval
+
+#     def run(self):
+#         while 1:
+#             if self._finished.isSet(): return
+#             self.task()
+            
+#             # sleep for interval or until shutdown
+#             self._finished.wait(self._interval)
+    
+#     def task(self):
+#         """The task done by this thread - override in subclasses"""
+#         self._func(*self._args)
+
+
+# class LogUsageTaskThread(TaskThread):
+# 	def __init__(self, interval=1):
+# 		TaskThread.__init__(self)
+# 		self.setInterval(interval)
+
+# 	def task(self):
+# 		print('updating..')
+# 		#usage[get_window_name()] += 1
+
+# def dummy():
+# 	print("hello")
+
+# def dummy2():
+# 	print("hi")
+
+# dummythread = LogUsageTaskThread()
+# dummythread.start()
+# print(usage)
