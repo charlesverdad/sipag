7b920aa130447ef3b6be66386198ec6dbc86a0d0
Charles Verdad <charles.verdad@gmail.com>
2018-06-19 19:20:30 +0800

    converts tabs to spaces

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import os
 config = {
-	'fn_format': '{date}_usage.json',
-	'log_interval': 1,  # 1 second
-	'save_interval': 10, # 1 minute
+    'fn_format': '{date}_usage.json',
+    'log_interval': 1,  # 1 second
+    'save_interval': 10, # 1 minute
 }
 
 # TODO: create this directory upon installation
 config['data_path'] = os.path.join(Path.home(), '.sipag')
 
 if os.name == 'nt':
-	config['data_path'] = os.path.join(Path.home(), 'AppData', 'Roaming', 'Likha', 'sipag')
+    config['data_path'] = os.path.join(Path.home(), 'AppData', 'Roaming', 'Likha', 'sipag')
--- sipag.py
+++ sipag.py
@@ -10,22 +10,22 @@ from config import config
 usage = Counter()
 
 def continuously_log():
-	global usage
-	lock = threading.Lock()
-	while True:
-		with lock:
-			log_usage(counter=usage)
-		sleep(config['log_interval'])
+    global usage
+    lock = threading.Lock()
+    while True:
+        with lock:
+            log_usage(counter=usage)
+        sleep(config['log_interval'])
 
 def continuously_save():
-	global usage
-	lock = threading.Lock()
-	while True:
-		with lock:
-			print(usage)
-			update_storage(usage)
-			usage = Counter()
-		sleep(config['save_interval'])
+    global usage
+    lock = threading.Lock()
+    while True:
+        with lock:
+            print(usage)
+            update_storage(usage)
+            usage = Counter()
+        sleep(config['save_interval'])
 
 logger_thread = Thread(target=continuously_log)
 logger_thread.daemon = True
@@ -40,4 +40,4 @@ sleep(2)
 print ('heya')
 sleep(3)
 while logger_thread.is_alive() and saver_thread.is_alive():
-	sleep(60)
+    sleep(60)
