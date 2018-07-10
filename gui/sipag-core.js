5c06b573a565b9b7c3c4939862e353a1ceb9f219
Charles Verdad <charles.verdad@gmail.com>
2018-07-10 10:50:03 +0800

    fix frontend and backend

--- gui/sipag-core.js
+++ gui/sipag-core.js
@@ -1,7 +1,36 @@
 monitor = require('active-window')
-
+config = require('./config')
+fs = require('fs')
 
 var counter = {}
+var timer = config.saveInterval
+
+function combineData(filepath){
+  // returns the combined counter data in
+  // `filepath and `counter` global variable
+
+  data = JSON.parse(fs.readFileSync(filepath))
+  for (key in counter){
+    if (data[key]){
+      data[key] += counter[key]
+    } else {
+      data[key] = counter[key]
+    }
+  }
+  return data
+}
+
+function writeData(filepath){
+  // Writes the the global variable `counter`
+  // to a json file located at `filepath`
+  data = combineData(filepath)
+  counter = {}
+  fs.writeFile(filepath, JSON.stringify(data), function(err){
+    if (err){
+      return console.log(err);
+    }
+  });
+}
 
 function logActiveWindow(window){
 
@@ -14,10 +43,18 @@ function logActiveWindow(window){
   }
 
   console.log(counter)
+  console.log(config.appPath)
+
+  if (timer == config.saveInterval){
+    timer = 0
+    writeData(config.getAppDataPath())
+  }
+  timer += 1
 }
 
 function startMonitor(){
-  monitor.getActiveWindow(logActiveWindow, -1, 1)
+  monitor.getActiveWindow(logActiveWindow, -1, 1) // ran every second infinitely.
+  // this assumes logActiveWindow() can finish in under 1 second.
 }
 
 module.exports = {
