21081a907a20d2545925baaf8b6a6a484ea6cf49
Charles Verdad <charles.verdad@gmail.com>
2018-07-20 23:40:39 +0800

    remove console logs

--- utils.js
+++ utils.js
@@ -23,7 +23,6 @@ function getAppDataPath(date=null) {
     // assume current date
     d = new Date();
     fn = d.getFullYear() + "_" + pad_num(d.getMonth()+1) + "_" + pad_num(d.getDate()+1) + "_" + "usage.json";
-    console.log('filename: ' + fn)
     return getAppPath() + fn;
   }
 }
